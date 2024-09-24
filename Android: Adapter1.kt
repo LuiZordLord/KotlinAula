53108945079feb30d472a9ec76482c84d4fa4945
Luiz Henrique Simonato <151886300+LuiZordLord@users.noreply.github.com>
2024-09-24 10:27:18 -0300

    Update Android: Adapter1.kt

--- Android: Adapter1.kt
+++ Android: Adapter1.kt
@@ -1,107 +1,107 @@
-package br.unipar.atividadeadapters01
+package com.example.atividadeadapters01
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etNomeAluno: EditText
+    private lateinit var spinnerAreaEscolha: Spinner
+    private lateinit var tvListaAlunos: TextView
+    private lateinit var tvContadorAlunos: TextView
+    private val listaAlunos = ArrayList<String>()
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        etNomeAluno = findViewById(R.id.etNomeAluno)
+        spinnerAreaEscolha = findViewById(R.id.spinnerAreaEscolha)
+        tvListaAlunos = findViewById(R.id.tvListaAlunos)
+        tvContadorAlunos = findViewById(R.id.tvContadorAlunos)
+        val btnInserir: Button = findViewById(R.id.btnInserir)
+        val btnZerar: Button = findViewById(R.id.btnZerar)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaAlunos)
+
+        btnInserir.setOnClickListener {
+            val nomeAluno = etNomeAluno.text.toString()
+            val areaEscolha = spinnerAreaEscolha.selectedItem.toString()
+            val dataAtual = SimpleDateFormat("dd/MM", Locale.getDefault()).format(Date())
+
+            if (nomeAluno.isNotEmpty()) {
+                listaAlunos.add("$nomeAluno - $areaEscolha - $dataAtual")
+                atualizarLista()
+            }
         }
 
-        val txtResultadoNome = findViewById<EditText>(R.id.txtResultadoNome)
-        val btnInserir = findViewById<Button>(R.id.btnInserir)
-        val txtResultadoArea = findViewById<EditText>(R.id.txtResultadoArea)
-        val btnZerar = findViewById<Button>(R.id.btnZerar)
-        val txtResultadoAlunos = findViewById<TextView>(R.id.txtResultadoAlunos)
+        btnZerar.setOnClickListener {
+            listaAlunos.clear()
+            atualizarLista()
+        }
+    }
 
+    private fun atualizarLista() {
+        tvListaAlunos.text = listaAlunos.joinToString("\n")
+        tvContadorAlunos.text = "${listaAlunos.size} Alunos"
     }
 }
 
+
 /////
 
-<?xml version="1.0" encoding="utf-8"?>
-<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:tools="http://schemas.android.com/tools"
-    android:id="@+id/main"
     android:layout_width="match_parent"
     android:layout_height="match_parent"
+    android:orientation="vertical"
+    android:padding="16dp"
     tools:context=".MainActivity">
 
-    <LinearLayout
-        android:layout_width="wrap_content"
-        android:layout_height="0dp"
-        android:orientation="vertical"
-        app:layout_constraintEnd_toEndOf="parent"
-        tools:layout_editor_absoluteY="1dp">
+    <EditText
+        android:id="@+id/etNomeAluno"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:hint="Nome Aluno" />
 
-        <TextView
-            android:id="@+id/txtNome"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:text="Nome Aluno:" />
-
-        <EditText
-            android:id="@+id/txtResultadoNome"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:ems="10"
-            android:inputType="text" />
-
-        <TextView
-            android:id="@+id/txtArea"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:text="Área de Escolha:" />
-
-        <EditText
-            android:id="@+id/txtResultadoArea"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:ems="10"
-            android:inputType="text" />
-
-        <ListView
-            android:layout_width="wrap_content"
-            android:layout_height="365dp">
-
-        </ListView>
-
-    </LinearLayout>
+    <Spinner
+        android:id="@+id/spinnerAreaEscolha"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:entries="@array/areas_escolha" />
 
     <Button
         android:id="@+id/btnInserir"
-        android:layout_width="122dp"
-        android:layout_height="47dp"
-        android:text="Inserir"
-        tools:layout_editor_absoluteX="211dp"
-        tools:layout_editor_absoluteY="87dp" />
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Inserir" />
 
     <TextView
-        android:id="@+id/txtResultadoAlunos"
-        android:layout_width="289dp"
-        android:layout_height="60dp"
-        tools:layout_editor_absoluteX="61dp"
-        tools:layout_editor_absoluteY="566dp" />
+        android:id="@+id/tvListaAlunos"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Lista de Alunos" />
+
+    <TextView
+        android:id="@+id/tvContadorAlunos"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="0 Alunos" />
 
     <Button
         android:id="@+id/btnZerar"
-        android:layout_width="237dp"
-        android:layout_height="54dp"
-        android:text="zerar"
-        tools:layout_editor_absoluteX="88dp"
-        tools:layout_editor_absoluteY="656dp" />
-
-</androidx.constraintlayout.widget.ConstraintLayout>
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="ZERAR"
+        android:background="#FF0000"
+        android:textColor="#FFFFFF" />
+</LinearLayout>
